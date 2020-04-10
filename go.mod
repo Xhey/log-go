267ed9f09caac08133746090f02fd148569a0649
yonggang shang <luo_yu_xia@163.com>
2020-04-10 19:28:36 +0800

    log fmt

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Xhey/log-go
+
+go 1.13
--- log.go
+++ log.go
@@ -1,23 +1,64 @@
-package xlog
+package public
 
-var logInst *logger
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"strings"
+	"time"
+)
 
-func Init(level string) {
-	logInst = newLogger(level)
+type Xlog struct {
+	LogLevel int
 }
 
-func Debug(format string, v ...interface{}) {
-	logInst.doLog(DEBUG, format, v...)
+const (
+	DEBUG = iota
+	INFO
+	ERROR
+	FATAL
+)
+
+var (
+	logLevel map[string]int
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags)
+	logLevel = make(map[string]int)
+	logLevel["DEBUG"] = DEBUG
+	logLevel["INFO"] = INFO
+	logLevel["ERROR"] = ERROR
+	logLevel["FATAL"] = FATAL
 }
 
-func Info(format string, v ...interface{}) {
-	logInst.doLog(INFO, format, v...)
+func (h *Xlog) dolog(level string, format string, v ...interface{}) {
+	if h.LogLevel > logLevel[level] {
+		return
+	}
+	pc, fn, line, _ := runtime.Caller(2)
+	names := strings.Split(fn, "/")
+	name := names[len(names)-1]
+	f := runtime.FuncForPC(pc)
+	logTime := time.Now().Format("2006/01/02 15:04:05")
+	prefix := fmt.Sprintf("%s [%s]: %s, <%s, %d>, ", logTime, level, name, f.Name(), line)
+	strlog := fmt.Sprintf(prefix+format, v...)
+
+	fmt.Println(strlog)
 }
 
-func Error(format string, v ...interface{}) {
-	logInst.doLog(ERROR, format, v...)
+func (h *Xlog) Debug(format string, v ...interface{}) {
+	h.dolog("DEBUG", format, v...)
 }
 
-func Fatal(format string, v ...interface{}) {
-	logInst.doLog(FATAL, format, v...)
+func (h *Xlog) Info(format string, v ...interface{}) {
+	h.dolog("INFO", format, v...)
+}
+
+func (h *Xlog) Error(format string, v ...interface{}) {
+	h.dolog("ERROR", format, v...)
+}
+
+func (h *Xlog) Fatal(format string, v ...interface{}) {
+	h.dolog("FATAL", format, v...)
 }
